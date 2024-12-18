ed26492e7c60e99ba290477a4735e9281f0d9bec
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-12-18 17:11:34 -0500

    Create S3_js.js

---
+++ S3_js.js
@@ -0,0 +1,26 @@
+
+const Minio = require('minio');
+
+// Set up MinIO Client 
+const minioClient = new Minio.Client({
+  endpoint: 'http://localhost',
+  port: 9000,
+  useSSL: false,
+  accessKey: 'minioadmin',
+  secretKey: 'minioadmin',
+  s3ForcePathStyle: true,  // Needed for MinIO
+  signatureVersion: 'v4'
+});
+
+// List all buckets in your MinIO server
+minioClient.listBuckets(function (err, buckets) {
+  if (err) {
+    return console.log('Error listing buckets:', err);
+  }
+
+  console.log('Buckets:');
+  buckets.forEach(bucket => {
+      console.log(bucket.name);
+ });
+});
+
